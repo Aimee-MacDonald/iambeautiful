ddf96ac50a43b2560d2c41793e48b393b6cfa379
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-02-12 23:03:59 +0200

    Placeholder images

--- public/scripts/prints.js
+++ public/scripts/prints.js
@@ -1,11 +1,9 @@
 var slider = document.getElementById("page-cover");
 
 function closeSlider(){
-  slider.style.width = "0";
   slider.style.height = "0";
 }
 
 function openSlider(index){
-  slider.style.width = "100%";
   slider.style.height = "100%";
 }
