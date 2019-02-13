c207e14e8f68e7e2a2cc25b1523f8ea8f9f71264
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-02-13 10:48:07 +0200

    Slider Functionality

--- public/scripts/prints.js
+++ public/scripts/prints.js
@@ -1,9 +1,33 @@
 var slider = document.getElementById("page-cover");
+var sliderImages = document.getElementsByClassName("slider-image");
+var sliderIndex;
+
+function openSlider(index){
+  slider.style.height = "100%";
+  sliderIndex = index;
+  updateSlider();
+}
 
 function closeSlider(){
   slider.style.height = "0";
 }
 
-function openSlider(index){
-  slider.style.height = "100%";
+function previousSlide(){
+  sliderIndex--;
+  if(sliderIndex < 0) sliderIndex += sliderImages.length;
+  updateSlider();
+}
+
+function nextSlide(){
+  sliderIndex++;
+  if(sliderIndex > sliderImages.length - 1) sliderIndex -= sliderImages.length;
+  updateSlider();
+}
+
+function updateSlider(){
+  for(var i = 0; i < sliderImages.length; i++){
+    sliderImages[i].style.width = "0";
+  }
+
+  sliderImages[sliderIndex].style.width = "100%";
 }
