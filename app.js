22b61f2b5775a374045dd20ab6a97f655a787504
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-02-25 12:02:42 +0200

    Admin Pages

--- app.js
+++ app.js
@@ -8,7 +8,31 @@ app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/public/views/index.html");
 });
 
-app.get("/prints", (req, res) => {
+app.get("/admin", (req, res) => {
+  res.redirect("/admin/register");
+});
+
+app.get("/admin/login", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/views/admin-login.html");
+});
+
+app.get("/admin/register", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/views/admin-register.html");
+});
+
+app.get("/admin/pages/home", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/admin/pages/blog", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/views/blog.html");
+});
+
+app.get("/admin/pages/postcards", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/views/postcards.html");
+});
+
+app.get("/admin/pages/prints", (req, res) => {
   res.status(200).sendFile(__dirname + "/public/views/prints.html");
 });
 
