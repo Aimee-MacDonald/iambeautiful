64fc4068aa4a3237eb42907f314406a38c5a79a6
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-03-03 19:59:56 +0200

    Local Images on Prints Page

--- public/scripts/prints.js
+++ public/scripts/prints.js
@@ -1,33 +1,47 @@
-var slider = document.getElementById("page-cover");
-var sliderImages = document.getElementsByClassName("slider-image");
-var sliderIndex;
+var el_prints = document.getElementById("prints");
 
-function openSlider(index){
-  slider.style.height = "100%";
-  sliderIndex = index;
-  updateSlider();
-}
+var url_prints = [
+  "FLOWER_IN_THE_DESERT_1.jpg",
+  "FLOWER_IN_THE_DESERT_2_copy.jpg",
+  "HIMBA_1.jpg",
+  "HIMBA_2_copy.jpg",
+  "HIMBA_3_copy.jpg",
+  "HIMBA_4_copy.jpg",
+  "MAASAI_Odyssey_1_copy.jpg",
+  "MAASAI_Odyssey_2_copy.jpg",
+  "Maasai_Odyssey_3_copy.jpg",
+  "Maasai_Odyssey_4_copy.jpg",
+  "Maasai_Odyssey_5_copy.jpg",
+  "Maasai_Odyssey_6_copy.jpg",
+  "PEOPLE_OF_THE_RAIN_1_copy.jpg",
+  "PEOPLE_OF_THE_RAIN_2_copy.jpg",
+  "PEOPLE_OF_THE_RAIN_3_copy.jpg",
+  "PEOPLE_OF_THE_RAIN_4_copy.jpg"
+]
+/*
+var url_prints = [
+  "https://imgur.com/omN38Za",
+  "https://imgur.com/5NOH5c6",
+  "https://imgur.com/UhaPolz",
+  "https://imgur.com/UecNTPX",
+  "https://imgur.com/vaS969c",
+  "https://imgur.com/afYIDtR",
+  "https://imgur.com/iUIcTGe",
+  "https://imgur.com/Qbj2Yjb",
+  "https://imgur.com/hukvpQV",
+  "https://imgur.com/EmCpxpf",
+  "https://imgur.com/TdwXfFs",
+  "https://imgur.com/1EhTX27",
+  "https://imgur.com/iNNqzJy",
+  "https://imgur.com/ACuc92j",
+  "https://imgur.com/Nh68MaA",
+  "https://imgur.com/wZt2rod",
+];
+*/
 
-function closeSlider(){
-  slider.style.height = "0";
-}
-
-function previousSlide(){
-  sliderIndex--;
-  if(sliderIndex < 0) sliderIndex += sliderImages.length;
-  updateSlider();
-}
-
-function nextSlide(){
-  sliderIndex++;
-  if(sliderIndex > sliderImages.length - 1) sliderIndex -= sliderImages.length;
-  updateSlider();
-}
-
-function updateSlider(){
-  for(var i = 0; i < sliderImages.length; i++){
-    sliderImages[i].style.width = "0";
-  }
-
-  sliderImages[sliderIndex].style.width = "100%";
+for(var i = 0; i < url_prints.length; i++){
+  var el_print = document.createElement("div");
+  el_print.classList = "print";
+  el_print.style.backgroundImage = "url(/images/temp/" + url_prints[i] + ")";
+  el_prints.append(el_print);
 }
