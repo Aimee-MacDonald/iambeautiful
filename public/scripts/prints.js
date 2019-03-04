0a83857897b28db6c7203ac56fad3fca13bd0275
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-03-04 20:04:46 +0200

    Prints online Images

--- public/scripts/prints.js
+++ public/scripts/prints.js
@@ -1,47 +1,27 @@
 var el_prints = document.getElementById("prints");
 
 var url_prints = [
-  "FLOWER_IN_THE_DESERT_1.jpg",
-  "FLOWER_IN_THE_DESERT_2_copy.jpg",
-  "HIMBA_1.jpg",
-  "HIMBA_2_copy.jpg",
-  "HIMBA_3_copy.jpg",
-  "HIMBA_4_copy.jpg",
-  "MAASAI_Odyssey_1_copy.jpg",
-  "MAASAI_Odyssey_2_copy.jpg",
-  "Maasai_Odyssey_3_copy.jpg",
-  "Maasai_Odyssey_4_copy.jpg",
-  "Maasai_Odyssey_5_copy.jpg",
-  "Maasai_Odyssey_6_copy.jpg",
-  "PEOPLE_OF_THE_RAIN_1_copy.jpg",
-  "PEOPLE_OF_THE_RAIN_2_copy.jpg",
-  "PEOPLE_OF_THE_RAIN_3_copy.jpg",
-  "PEOPLE_OF_THE_RAIN_4_copy.jpg"
-]
-/*
-var url_prints = [
-  "https://imgur.com/omN38Za",
-  "https://imgur.com/5NOH5c6",
-  "https://imgur.com/UhaPolz",
-  "https://imgur.com/UecNTPX",
-  "https://imgur.com/vaS969c",
-  "https://imgur.com/afYIDtR",
-  "https://imgur.com/iUIcTGe",
-  "https://imgur.com/Qbj2Yjb",
-  "https://imgur.com/hukvpQV",
-  "https://imgur.com/EmCpxpf",
-  "https://imgur.com/TdwXfFs",
-  "https://imgur.com/1EhTX27",
-  "https://imgur.com/iNNqzJy",
-  "https://imgur.com/ACuc92j",
-  "https://imgur.com/Nh68MaA",
-  "https://imgur.com/wZt2rod",
+  "https://i.imgur.com/omN38Za.jpg",
+  "https://i.imgur.com/5NOH5c6.jpg",
+  "https://i.imgur.com/UhaPolz.jpg",
+  "https://i.imgur.com/UecNTPX.jpg",
+  "https://i.imgur.com/vaS969c.jpg",
+  "https://i.imgur.com/afYIDtR.jpg",
+  "https://i.imgur.com/iUIcTGe.jpg",
+  "https://i.imgur.com/Qbj2Yjb.jpg",
+  "https://i.imgur.com/hukvpQV.jpg",
+  "https://i.imgur.com/EmCpxpf.jpg",
+  "https://i.imgur.com/TdwXfFs.jpg",
+  "https://i.imgur.com/1EhTX27.jpg",
+  "https://i.imgur.com/iNNqzJy.jpg",
+  "https://i.imgur.com/ACuc92j.jpg",
+  "https://i.imgur.com/Nh68MaA.jpg",
+  "https://i.imgur.com/wZt2rod.jpg",
 ];
-*/
 
 for(var i = 0; i < url_prints.length; i++){
   var el_print = document.createElement("div");
   el_print.classList = "print";
-  el_print.style.backgroundImage = "url(/images/temp/" + url_prints[i] + ")";
+  el_print.style.backgroundImage = "url(" + url_prints[i] + ")";
   el_prints.append(el_print);
 }
